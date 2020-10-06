59079cab831f1ddc7b6c385fde380570eaaea1cb
Amirt04 <72074371+Amirt04@users.noreply.github.com>
2020-10-06 18:05:05 +0600

    Shooter

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 100;
+    
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
