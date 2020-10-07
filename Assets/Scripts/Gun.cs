b0456f7b0dd397847a92204fee81c6638aea7152
Amirt04 <72074371+Amirt04@users.noreply.github.com>
2020-10-07 18:22:06 +0600

    Shooter

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,6 +8,9 @@ public class Gun : MonoBehaviour
     public float range = 100;
 
     public Camera fpsCam;
+    public ParticleSystem shootEffect;
+    public GameObject bulletEffect;
+    public float impactForce = 60;
     void Start()
     {
         
@@ -22,10 +25,20 @@ public class Gun : MonoBehaviour
     }
     public void Shoot()
     {
+        shootEffect.Play();
         RaycastHit hit;
 
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
+            GameObject effect = Instantiate(bulletEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(effect, 1);
+
+            Rigidbody rigidbody = hit.collider.GetComponent<Rigidbody>();
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
             Target target = hit.collider.GetComponent<Target>();
             if(target != null)
             {
