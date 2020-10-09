bbd32397e3e750b50b3f2ffef87b02878d793db2
Amirt04 <72074371+Amirt04@users.noreply.github.com>
2020-10-09 18:26:17 +0600

    Shooter

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealthLife = 100;
+    public float curHealthLife;
+    float HealthDrain = 1f;
+    public Image healthUI;
+    void Start()
+    {
+        curHealthLife = maxHealthLife;
+    }
+    void Update()
+    {
+        if (curHealthLife > 0)
+        {
+            curHealthLife -= HealthDrain * Time.deltaTime;
+        }
+
+        if (curHealthLife <= 0)
+        {
+            curHealthLife = 0;
+        }
+
+        healthUI.transform.localScale = new Vector3(
+    curHealthLife / maxHealthLife,
+    healthUI.transform.localScale.y,
+    healthUI.transform.localScale.z
+    );
+
+        if (curHealthLife <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/Medkit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medkit : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void UseBattery()
+    {
+        player.GetComponentInChildren<player>().AddHealth();
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+}
